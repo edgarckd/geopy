60a648cc53b5e635523b8ddc3ece8aae5e0f9c5f
Edgar Paez <edgarpaez@localhost.localdomain>
2021-05-26 01:13:49 -0500

    se extraen latitud y longitud

--- geopy.py
+++ geopy.py
@@ -1,1 +1,12 @@
 import requests as rq
+import json as js
+
+
+parametros = { "key" : "Lyrv1vpGKjuNERqHw0oYFer94QqSc0nn" , "location" : "calle 36 #26-64 la arboledad soledad"}
+response = rq.get("http://www.mapquestapi.com/geocoding/v1/address", params=parametros)
+data = js.loads(response.text)["results"]
+lat = data[0]['locations'][0]['latLng']['lat']
+lng = data[0]['locations'][0]['latLng']['lng']
+
+print(lat)
+print(lng)
